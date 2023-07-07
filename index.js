364fd62b62d435f6b54e027f4865c7514903a08e
Illia Chernyk <ilyachernykan@gmail.com>
2023-07-07 20:13:09 +0300

    Final commit

--- index.js
+++ index.js
@@ -1,29 +1,66 @@
-// task 1
-let value1;
-let value2;
+// task 1.1
+const value1 = 11;
+const value2 = -0;
 let result;
 if (value1 > 0 && value2 > 0 || value1 < 0 && value2 < 0) {
-    console.log(result = value1 * value2);
+    result = value1 * value2;
 } else if (value1 > 0 && value2 < 0 || value1 < 0 && value2 > 0) {
-    console.log(result = value1 / value2);
-} else if (value1 == 0 && value2 !== 0 ) {
-    console.log(result = value2);
-} else if (value1 !== 0 && value2 == 0) {
-    console.log(result = value1);
+    result = value1 / value2;
+} else if (value1 === 0 && value2 !== 0 ) {
+    result = value2;
+} else if (value1 !== 0 && value2 === 0) {
+    result = value1;
 } else {
-    console.log(result = NaN);
+    result = NaN;
 }
+console.log(result);
+
+
+// task 1.2
+if (value1 > 0 && value2 > 0 || value1 < 0 && value2 < 0) {
+    result = value1 * value2;
+} else if (value1 > 0 && value2 < 0 || value1 < 0 && value2 > 0) {
+    result = value1 / value2;
+} else if (value1 === 0 && value2 === 0) {
+    result = NaN;
+} else {
+    if (value2 !== 0) {
+        result = value2; 
+    } else {
+        result = value1;
+    }
+}
+console.log(result);
+
+// task 1.3
+if (value1 === 0 && value2 === 0) {
+    result = NaN;
+} else if(value2 === 0){
+    result = value1;
+} else if (value1 === 0 ) {
+    result = value2;
+} else if (value1 / value2 > 0) {
+    result = value1 * value2;  
+} else {
+    result = value1 / value2;
+}
+console.log(result);
+
 
 // task 2
-    let minValue;
-    let number;
-    let divider1; 
-    let divider2;
+    const minNumber = 0;
+    const number = 28;
+    const divider1 = 4; 
+    const divider2 = 7;
     let res;
 
-    if (number < minValue || number % 2 !==0 || number % divider1 !== 0 || number % divider2 !== 0) {
+    if (number < minNumber || number % 2 !==0 || number % divider1 !== 0 || number % divider2 !== 0) {
         res = 'lose';
     } else {
         res = 'win';
 }
 console.log(res);
+
+
+
+
